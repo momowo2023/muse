700391244c6478c0209dc63395b74846e44cb23a
mowei <mowei.wmw@gmail.com>
2024-11-21 22:23:45 +0100

    cv update

---
+++ js/include.js
@@ -0,0 +1,20 @@
+function loadHTML(id, file) {
+    fetch(file)
+      .then((response) => {
+        if (!response.ok) throw new Error(`Failed to load ${file}`);
+        return response.text();
+      })
+      .then((html) => {
+        document.getElementById(id).innerHTML = html;
+      })
+      .catch((error) => {
+        console.error(error);
+        document.getElementById(id).innerHTML = `<p>Error loading ${file}</p>`;
+      });
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    loadHTML("header", "pages/header.html");
+    loadHTML("footer", "pages/footer.html");
+  });
+  
